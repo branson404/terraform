ead5f7b4999e00f97144104e9333cc18128ce7ae
Mohammed <78994175+branson404@users.noreply.github.com>
2026-02-18 17:46:59 +0000

    implemented modular way of resources

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-    ami = var.ami_value
-    instance_type = var.instance_type_value
-    associate_public_ip_address = true
+module "ec2_instance" {
+  source = "./modules/ec2"
+  ami_value = "ami-053b0d53c279acc90" 
+  instance_type_value = "t2.micro"
 }
